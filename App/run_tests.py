6a3f108bda62a5eff05b7ee0766cd40381debf51
UnVeh <159423384+UnVeh@users.noreply.github.com>
2024-05-04 11:53:52 +0300

    Update run_tests.py

--- App/run_tests.py
+++ App/run_tests.py
@@ -1,6 +1,12 @@
+import sys
+import os
 import unittest
+
+# Добавляем корневую директорию App в sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 if __name__ == '__main__':
     test_loader = unittest.TestLoader()
-    test_suite = test_loader.discover('App/tests', pattern='test_*.py')
+    test_suite = test_loader.discover('tests', pattern='test_*.py')
     unittest.TextTestRunner().run(test_suite)
+
