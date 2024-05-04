560133f718f9b3cd848306c9ffeace74d0dc6b1d
UnVeh <159423384+UnVeh@users.noreply.github.com>
2024-05-04 17:13:41 +0300

    Delete App/run_tests.py

--- App/run_tests.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-if __name__ == '__main__':
-    test_loader = unittest.TestLoader()
-    test_suite = test_loader.discover('tests', pattern='test_*.py')
-    unittest.TextTestRunner().run(test_suite)
-
-
