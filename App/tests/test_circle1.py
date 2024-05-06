f809c7f36c1202c35e2ddd1513830e89470982b6
UnVeh <159423384+UnVeh@users.noreply.github.com>
2024-05-06 12:38:40 +0300

    Update test_circle1.py

--- App/tests/test_circle1.py
+++ App/tests/test_circle1.py
@@ -6,7 +6,6 @@ import os
 
 class TestSketch(unittest.TestCase):
     def setUp(self):
-        # Изменяем рабочую директорию на уровень выше, чтобы папка App была доступна.
         os.chdir(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
         self.root = Tk()
